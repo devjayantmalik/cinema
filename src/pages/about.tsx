870409102d3726cf63c698d2add508ecd037af8a
hat <dev.jayantmalik@gmail.com>
2021-07-05 12:04:04 +0530

    added ui

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ContactUsSection } from "../components/ContactUsSection";
-import { DevelopBharatSection } from "../components/DevelopBharatSection";
 import { Page } from "../components/layout";
 import { StatsSection } from "../components/StatsSection";
 
